fd4ff53ee3f3ec2a5a7ccee692c9cd5dc5d98a8f
akbars95 <Dmitri.Minzat@endava.com>
2017-02-08 14:40:39 +0200

    add apache-camel project

---
+++ java/real-projects/eip/apache-camel/src/main/java/com/mtsmda/camel/withoutspring/annotation/RunInlineCamelContext.java
@@ -0,0 +1,21 @@
+package com.mtsmda.camel.withoutspring.annotation;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ * Created by dminzat on 2/4/2017.
+ */
+public class RunInlineCamelContext {
+
+    public static void main(String[] args) {
+        CamelContext camelContext = new DefaultCamelContext();
+        try{
+
+        }
+        catch (Exception e){
+
+        }
+    }
+
+}
